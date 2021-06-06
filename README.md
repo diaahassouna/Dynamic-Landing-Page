2a0deb1ae2e851483fe9c36da145bade5b99e1c7
diaahassouna <sightmare@gmail.com>
2021-06-06 11:14:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Landing Page
+### _Utilizing JavaScript to make dynamic experience_
+
+The Landing Page Project is an example of how to harness the power of JavaScript to manipulate the DOM by the following:
+
+- Creating new elements just like the top menu and its styles.
+- Editing styles and functionality for activated and deactivated elements.
+- Automatic scrolling on-click.
+
+## Features
+
+- A Dynamically created navigation menu on the header, and no matter how many sections will be added in `index.html` the menu will adapt to it in automatic fashion.
+- The page automatically scrolls to the desired section when you click on its peer on the navigation menu.
+- The activated section and its peer on the navigation menu change their styles in order to exhibit their active status.
+- The previous activated section and its peer return to their native style.
+
+## Tech
+
+- The original `index.html` and `css/styles.css` are provided by [Udacity].
+- The original `index.html` provided by [Udacity] has 3 sections and 2 other sections were added to test the functionality of adding new navigation menu items automatically.
+- All other development features -including CSS new rules- are done in `js/app.js`.
+- Developed by [Diaa Hassouna] - a JavaScript Project Submission.
+
+   [Udacity]: <https://github.com/udacity/fend/tree/refresh-2019>
+   [Diaa Hassouna]: <https://github.com/diaahassouna>
+   
---
+++ js/app.js
@@ -0,0 +1,88 @@
+// Make the Landing Page load dynamically from the start
+document.addEventListener('DomContentLoaded', navBar());
+document.addEventListener('DOMContentLoaded', navBarStyle());
+
+// Create the Navigation Menu on Top and activate current clicked item
+function navBar() {
+    const section = document.getElementsByTagName('section');
+
+    for (let i = 0; i < section.length; i++) {
+        let list = document.createElement('li');
+        let anchor = document.createElement('a');
+        let sectionName = section[i].getAttribute('data-nav');
+        let sectionNameAttribute = sectionName.replace(/\s/g, '').toLowerCase();
+
+        anchor.innerText = sectionName;
+        anchor.setAttribute('href', `#${sectionNameAttribute}`);
+        anchor.setAttribute('id', 'link_no' + (i + 1))
+        list.appendChild(anchor);
+        document.getElementById('navbar__list').appendChild(list);
+        document.getElementById('link_no' + (i + 1)).addEventListener('click', function() {
+            scrollEvent(i + 1);
+            sectionActivate(i + 1);
+            navActivate(i + 1);
+        }) 
+    }
+}
+
+// Scroll Event on Click
+function scrollEvent(no) {
+    let section = document.getElementById('section' + no);
+    let position = section.offsetTop;
+    event.preventDefault();
+    window.scrollTo({
+        left: 0,
+        top: position,
+        behavior: 'smooth'
+    })
+}
+
+//Dynamic Style for Navigation Menu
+function navBarStyle () {
+    let sheet = window.document.styleSheets[1];
+    let styles = `a {
+        display: flex;
+        flex-direction: row;
+        align-items: stretch;
+        color: #000;
+        text-decoration: none;
+        margin: 0 0.5em 0 0.5em;
+        padding: 0.5em;
+        background-color: rgb(220, 220, 220);
+        font-size: large;
+        transform:translateX(-0.5em);
+    }`;
+    let hover = `a:hover {
+        background-color: rgb(47, 0, 255);
+        color: #fff;
+    }`
+    sheet.insertRule(styles, sheet.cssRules.length);
+    sheet.insertRule(hover, sheet.cssRules.length);
+}
+
+// Activate section on click and deactivate previous one
+function sectionActivate(no) {
+    let activeClass = 'your-active-class';
+    let activeSection = document.getElementsByClassName(activeClass)[0];
+    let newActiveSection = document.getElementById('section' + no);
+
+    activeSection.removeAttribute('class');
+    newActiveSection.setAttribute('class', activeClass);
+}
+
+// Activate clicked navigation menu element
+function navActivate(no) {
+    let activeNav = document.getElementById('link_no' + no);
+    let otherNavs = document.getElementsByTagName('a');
+
+    for (i = 0; i <otherNavs.length; i++) {
+        if (otherNavs[i].style.backgroundColor == "rgb(47, 0, 255)") {
+            oldNav = otherNavs[i];
+            oldNav.style.backgroundColor = "rgb(220, 220, 220)";
+            oldNav.style.color = "#000";           
+        }
+    }
+
+    activeNav.style.backgroundColor = "rgb(47, 0, 255)";
+    activeNav.style.color = "#fff";
+}
